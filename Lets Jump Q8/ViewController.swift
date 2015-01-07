1a93df8b1231ac7e75ba31b6c6f475fbba91da2f
noureldien <mediahouse.it@gmail.com>
2015-01-08 01:13:00 +0200

    merge

--- Lets Jump Q8/ViewController.swift
+++ Lets Jump Q8/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var forRentBtn: UIButton!
+    @IBOutlet weak var forSaleBtn: UIButton!
+    @IBOutlet weak var haflat: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        forRentBtn.titleLabel?.textAlignment = NSTextAlignment.Center;
+        forSaleBtn.titleLabel?.textAlignment = NSTextAlignment.Center;
+        haflat.titleLabel?.textAlignment = NSTextAlignment.Center;
+        
     }
+    
+    
+   
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+   
 }
 
